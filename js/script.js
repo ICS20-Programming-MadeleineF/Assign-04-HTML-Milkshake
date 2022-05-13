4e0985231229004c55b12af1a6a8fcf39e9bea4a
Madeleine Forgeron <99274168+MadeleineForgeron@users.noreply.github.com>
2022-05-13 15:05:16 +0000

    cont.

--- js/script.js
+++ js/script.js
@@ -17,23 +17,33 @@ const LRG_PRICE = 7.58
 const EXT_LRG_PRICE = 8.99
 const MIX_PRICE = 1.50
 const SNK_PRICE = 5.00
+const NONE_PRICE = 0.00
   
 function displayCost() {
   	// initialize variables
   let sizePrice = 0
-  let mixPrice = 0
+  let mixPrice1 = 0
+  let mixPrice2 = 0
+  let mixPrice3 = 0
   let snkPrice = 0
 	// get age and day of the week
+  let select1 = document.getElementById('size');
 	let size = select1.options[select1.selectedIndex].value;
-  	let select1 = document.getElementById('size');
+  
+  let select2 = document.getElementById('mixins1');
   let mixins1 = select2.options[select2.selectedIndex].value;
-  	let select2 = document.getElementById('mixins1');
+  
+  let select3 = document.getElementById('mixins2');
   let mixins2 = select3.options[select3.selectedIndex].value;
-  	let select3 = document.getElementById('mixins2');
+  
+
+  let select4 = document.getElementById('mixins3');
   let mixins3 = select4.options[select4.selectedIndex].value;
-  	let select4 = document.getElementById('mixins3');
-    let snacks = parseInt(document.getElementById('snacks').value);
-      	//  Milkshake size price
+  
+  
+  let snacks = parseInt(document.getElementById('snacks').value);
+  
+  //  Milkshake size price
   if (size == "Mini")  {
 		sizePrice = MINI_PRICE 
 	}
@@ -46,113 +56,114 @@ function displayCost() {
   else  if (size == "Large")  {
 		sizePrice = LRG_PRICE 
 	}
-	else {
+	else { 
 		sizePrice = EXT_LRG_PRICE
 	}
 
+
   // mixin 1 price 
-   if (mixins == "Strawberries") {
-		mixPrice = MIX_PRICE
+   if (mixins1 == "Strawberries") {
+		mixPrice1 = MIX_PRICE
 	}
-	else if (mixins == "Bananas") {
-		mixPrice = MIX_PRICE
+	else if (mixins1 == "Bananas") {
+		mixPrice1 = MIX_PRICE
 	}
-  else if (mixins == "Blueberries") {
-		mixPrice = MIX_PRICE
+  else if (mixins1 == "Blueberries") {
+		mixPrice1 = MIX_PRICE
 	}
-	else if (mixins == "Cherries") {
-		mixPrice = MIX_PRICE
+	else if (mixins1 == "Cherries") {
+		mixPrice1 = MIX_PRICE
 	}
-	else if (mixins == "Crushed smarties") {
-		mixPrice = MIX_PRICE
+	else if (mixins1 == "Crushed smarties") {
+		mixPrice1 = MIX_PRICE
 	}
-	else if (mixins == "Cake chunks") {
-		mixPrice = MIX_PRICE
+	else if (mixins1 == "Cake chunks") {
+		mixPrice1 = MIX_PRICE
 	}
-  else if (mixins == "Whipped cream") {
-		mixPrice = MIX_PRICE
+  else if (mixins1 == "Whipped cream") {
+		mixPrice1 = MIX_PRICE
 	}
-	else if (mixins == "Sprinkles") {
-		mixPrice = MIX_PRICE
+	else if (mixins1 == "Sprinkles") {
+		mixPrice1 = MIX_PRICE
 	}
-	else if (mixins == "Oreo crumble") {
-		mixPrice = MIX_PRICE
+	else if (mixins1 == "Oreo crumble") {
+		mixPrice1 = MIX_PRICE
 	}
-	else if (mixins == "Cookie dough") {
-		mixPrice = MIX_PRICE
+	else if (mixins1 == "Cookie dough") {
+		mixPrice1 = MIX_PRICE
   }
 	else {
-		mixPrice = 0
-	}
+		mixPrice1 = 0
+  }
 
   // mixin 2 price 
    if (mixins2 == "Strawberries") {
-		mixPrice = MIX_PRICE
+		mixPrice2 = MIX_PRICE
 	}
 	else if (mixins2 == "Bananas") {
-		mixPrice = MIX_PRICE
+		mixPrice2 = MIX_PRICE
 	}
   else if (mixins2 == "Blueberries") {
-		mixPrice = MIX_PRICE
+		mixPrice2 = MIX_PRICE
 	}
 	else if (mixins2 == "Cherries") {
-		mixPrice = MIX_PRICE
+		mixPrice2 = MIX_PRICE
 	}
 	else if (mixins2 == "Crushed smarties") {
-		mixPrice = MIX_PRICE
+		mixPrice2 = MIX_PRICE
 	}
 	else if (mixins2 == "Cake chunks") {
-		mixPrice = MIX_PRICE
+		mixPrice2 = MIX_PRICE
 	}
   else if (mixins2 == "Whipped cream") {
-		mixPrice = MIX_PRICE
+		mixPrice2 = MIX_PRICE
 	}
 	else if (mixins2 == "Sprinkles") {
-		mixPrice = MIX_PRICE
+		mixPrice2 = MIX_PRICE
 	}
 	else if (mixins2 == "Oreo crumble") {
-		mixPrice = MIX_PRICE
+		mixPrice2 = MIX_PRICE
 	}
 	else if (mixins2 == "Cookie dough") {
-		mixPrice = MIX_PRICE
+		mixPrice2 = MIX_PRICE
   }
 	else  {
-		mixPrice = 0
+		mixPrice2 = 0
   }
 
   // mixin 3 price 
    if (mixins3 == "Strawberries") {
-		mixPrice = MIX_PRICE
+		mixPrice3 = MIX_PRICE
 	}
 	else if (mixins3 == "Bananas") {
-		mixPrice = MIX_PRICE
+		mixPrice3 = MIX_PRICE
 	}
   else if (mixins3 == "Blueberries") {
-		mixPrice = MIX_PRICE
+		mixPrice3 = MIX_PRICE
 	}
 	else if (mixins3 == "Cherries") {
-		mixPrice = MIX_PRICE
+		mixPrice3 = MIX_PRICE
 	}
 	else if (mixins3 == "Crushed smarties") {
-		mixPrice = MIX_PRICE
+		mixPrice3 = MIX_PRICE
 	}
 	else if (mixins3 == "Cake chunks") {
-		mixPrice = MIX_PRICE
+		mixPrice3 = MIX_PRICE
 	}
   else if (mixins3 == "Whipped cream")   {
-		mixPrice = MIX_PRICE
+		mixPrice3 = MIX_PRICE
 	}
 	else if (mixins3 == "Sprinkles") {
-		mixPrice = MIX_PRICE
+		mixPrice3 = MIX_PRICE
 	}
 	else if (mixins3 == "Oreo crumble") {
-		mixPrice = MIX_PRICE
+		mixPrice3 = MIX_PRICE
 	}
 	else if (mixins3 == "Cookie dough") {
-		mixPrice = MIX_PRICE
+		mixPrice3 = MIX_PRICE
   }
 	else  {
-		mixPrice = 0
+		mixPrice3 = 0
 	}
   
   // snack price 
@@ -170,10 +181,10 @@ function displayCost() {
 	}
   
   // Totals
-	let subtotal = sizePrice + mixPrice +  snkPrice; 
+	let subtotal = sizePrice + mixPrice1 + mixPrice2 + mixPrice3 +  snkPrice; 
   let tax = subtotal * HST;
   let total = subtotal + tax;
 	
   	// display the results
-  	document.getElementById('display-results').innerHTML  = "Your subtotal is $" + subtotal.toFixed(2) + ".<br>The amount of HST added is $" + tax.toFixed(2) + ".<br>Your total is $" + total.toFixed(2) + ".";
+  	document.getElementById('display-results').innerHTML  = "Subtotal is $" + subtotal.toFixed(2) + ".<br>HST is $" + tax.toFixed(2) + ".<br>Your total is $" + total.toFixed(2) + ".";
 }
